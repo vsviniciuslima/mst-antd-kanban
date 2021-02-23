15ff181b4968e8e6312c741e0c68e391bf3a26aa
vsviniciuslima <vsviniciuslima@usp.br>
2021-02-23 17:04:56 -0300

    implementing generateFiltersFromDataSet view

--- src/models/TaskStore.js
+++ src/models/TaskStore.js
@@ -1,6 +1,6 @@
 import { types } from 'mobx-state-tree'
 
-import Task from './Task'
+import Task from './Task.js'
 
 function slugify (x) {
   return encodeURIComponent(x.toLowerCase().replace(/\s+/gim, '-'))
@@ -22,19 +22,25 @@ const TaskStore = types
 /*         default:
           return self.tasks.filter(task => task.status === 'Aguardando execução' || task.status === 'Em execução') */
       }
+    },
+    generateFiltersFromDataSet (key) {
+      const notFilteredEntriesArray = []
+
+      self.tasks.forEach(task => { notFilteredEntriesArray.push(task[key]) })
+      const filteredEntriesArray = [...new Set(notFilteredEntriesArray)]
+
+      const uniqueEntries = []
+
+      filteredEntriesArray.forEach(entry => {
+        uniqueEntries.push({ text: entry, value: entry })
+      })
+
+      return uniqueEntries
     }
   }))
   .actions(self => ({
     addTask (Task) {
       self.tasks.push(Task)
-    },
-    getResponsaveis () {
-      const responsaveis = []
-
-      self.tasks.forEach(task => { responsaveis.push(task.responsavel) })
-      responsaveis.forEach(responsavel => { console.log(responsavel) })
-
-      return responsaveis
     }
   }))
 
